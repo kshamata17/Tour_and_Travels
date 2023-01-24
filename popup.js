5cbb6492e931e173aedcfc9c22230921a343548d
Project <kshamata@p.com>
2023-01-24 13:52:37 +0545

    update

--- popup.js
+++
@@ -1,6 +0,0 @@
-function toggle(){
-    var wrapper2 = document.getElementsByClassName("wrapper2");
-    wrapper2.classList.toggle('active');
-    var popup = document.getElementById("popup");
-    popup.classList.toggle('active');
-}
